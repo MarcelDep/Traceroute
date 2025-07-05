ec9e0b6d4eabf5412cd7dee06f528a96213cfc4f
Marcel Depta <marceld2005@gmail.com>
2025-07-06 00:44:01 +0200

    Working reading dest IP address

--- traceroute.c
+++ traceroute.c
@@ -5,26 +5,36 @@ traceroute utulize TTL (time to live) function that when packet's is TTL = 0 the
 "Time Exceeded" messege to source host. So traceroute sends packet to choosed host starting with TTL = 0 
 and incrise its value for each "Time Exceeded" message (this means for each router).*/
 
-// Variables
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+// Variables
 int TTL;
 
 struct ip_addresses
 {
-    char src_ip[12];
-    char dst_ip[12];
+    char src_ip[15];
+    char dst_ip[15];
 };
 
-// In this function we are going to get a source IP address of user PC and destination IP address user can choose
-
-struct ip_addresses get_IP_Addresses() {
-
-}
-
 int main() {
-    printf("Choose destination IP address.");
-
-return 0;
+    int n1, n2, n3, n4;
+    struct ip_addresses ip_a;
+    char * pointerToDstIp = ip_a.dst_ip;
+    printf("Choose destination IP address.\n");
+    scanf("%s", pointerToDstIp);
+    int properFormat = sscanf(ip_a.dst_ip, "%d.%d.%d.%d", &n1, &n2, &n3, &n4);
+    if (properFormat != 4) {
+        printf("You've used wrong IP address format. Proper format is [0-225].[0-225].[0-225].[0-225]\n");
+        exit(EXIT_FAILURE);
+        }
+        if ((n1 < 0 || n1 > 255) || (n2 < 0 || n2 > 255) || (n3 < 0 || n3 > 255) || (n4 < 0 || n4 > 255)) {
+        printf("Each octet has to have value between 0 and 255.\n");
+        exit(EXIT_FAILURE);
+        }
+        else {
+        printf("Gitówa\n");
+    }
+    return 0;
 }
