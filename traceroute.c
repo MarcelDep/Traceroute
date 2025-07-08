0051562792b284da022acad0de4b38dd038e0601
Marcel Depta <marceld2005@gmail.com>
2025-07-08 23:14:32 +0200

    Day no. 5

--- traceroute.c
+++ traceroute.c
@@ -8,33 +8,27 @@ and incrise its value for each "Time Exceeded" message (this means for each rout
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 // Variables
 int TTL = 1;
+char dstIp4[16];
+struct sockaddr_in dstadd;
 
-struct ip_addresses {
-    char src_ip[16];
-    char dst_ip[16];
-};
-
-struct ip_addresses getIpAddresses() {
+char * getIpAddresses() {
     // Choosing destination IP address
     int n1, n2, n3, n4;
-    // Variables for ip addresses 
-    char hostSrcIp[16];
-    char choosedDstIp[16];
-
-    struct ip_addresses ip_a = { 0 };
-
-    char * pointerToDstIp = choosedDstIp;
+    char * pointerToSrcIp;
+    char * pointerToDstIp = dstIp4;
     printf("Choose destination IP address.\n");
     // NOTE: scanf nie sprawdza długości bufora, do którego pisze - giga niebezpieczne! (ta funkcja zakłada, że ma odpowiednio dużo pamięci i robi co chce)
-    // NOTE: użyj read_line z biblioteki unixowejw
+    // NOTE: użyj read_line z biblioteki unixowej
     scanf("%s", pointerToDstIp);
-    printf("%s", choosedDstIp);
+    printf("%s", dstIp4);
     // Ensureing proper IP address format 
-    // TODO: zamiast %d, które oczekuje "dowolnej liczby" możesz użyć format specifier'a, który oczekuje explicite 8 bitowej liczby bez znaku [0, 255]
-    int properFormat = sscanf(choosedDstIp, "%d.%d.%d.%d", &n1, &n2, &n3, &n4);
+    int properFormat = sscanf(dstIp4, "%d.%d.%d.%d", &n1, &n2, &n3, &n4);
     if (properFormat != 4) {
         printf("You've used wrong IP address format. Proper format is [0-225].[0-225].[0-225].[0-225]\n");
         exit(EXIT_FAILURE);
@@ -43,17 +37,12 @@ struct ip_addresses getIpAddresses() {
         printf("Each octet has to have value between 0 and 255.\n");
         exit(EXIT_FAILURE);
     }
-    // If user have given proper destination IP address, then program will retvie their source IP address
-    else {
-        
-    }
-    // struct ip_addresses obtainedIpAddresses = {.src_ip = ip_a.hostSrcIp,
-    //                                            .dst_ip = ip_a.choosedDstIp};
-    return obtainedIpAddresses;
+    // Converting inputted IP address from char to decimal value
+    inet_pton(AF_INET, dstIp4, &(dstadd.sin_addr));
+    printf("%ld", dstadd.sin_addr);
+    return dstIp4;
 }
 
 int main() {
-    struct ip_addresses ip;
-    ip = getIpAddresses();
-    printf("%s %s", ip.src_ip, ip.dst_ip);
+
 }
