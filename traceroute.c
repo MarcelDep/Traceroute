ebc5cd1a72d8cc0c71a9e6b42c2a8cd574dab03a
MarcelDep <marceld2005@gmail.com>
2025-10-21 13:55:17 +0200

    Corrected json file

--- traceroute.c
+++ traceroute.c
@@ -40,18 +40,13 @@ static int TTL = 1;
 static char dstIp4[16];
 static struct addrinfo hints = { 0 };
 static struct addrinfo *res;
-<<<<<<< HEAD
 const char * destination;
-=======
-static struct in_addr dstIpAddr;
->>>>>>> cf85765d2127e4aefd8a7d227c60e219b1c35300
 
 // CTRL + ALT + Strzałka -> dodaj nowy kursor
 // CTRL + D -> dodaj kursor przy matchującym wzorcu
 // CTRL + R -> mądry znajdź i zamień (refactor)
 
 // Function to retrieve destination IPv4 address user can choose 
-<<<<<<< HEAD
 // const char * getIpAddresses() {
 //     // Choosing destination IP address
 //     int n1, n2, n3, n4;
@@ -113,47 +108,6 @@ int sendEchoMess(const char * hostname) {
     // At the end we have to clean up result struct info
     freeaddrinfo(result);
     return 0;
-=======
-const char * getIpAddresses() {
-    // Choosing destination IP address
-    int n1, n2, n3, n4;
-    char * pointerToDstIp = dstIp4;
-    printf("Choose destination IP address.\n");
-    // NOTE: scanf nie sprawdza długości bufora, do którego pisze - giga niebezpieczne! (ta funkcja zakłada, że ma odpowiednio dużo pamięci i robi co chce)
-    // NOTE: użyj read_line z biblioteki unixowej
-    scanf("%s", pointerToDstIp);
-    // Ensureing proper IP address format 
-    int properFormat = sscanf(dstIp4, "%d.%d.%d.%d", &n1, &n2, &n3, &n4);
-    if (properFormat != 4) {
-        printf("You've used wrong IP address format. Proper format is [0-225].[0-225].[0-225].[0-225]\n");
-        // printf("Trying to resolve domain name...");
-        exit(EXIT_FAILURE);
-    }
-    if ((n1 < 0 || n1 > 255) || (n2 < 0 || n2 > 255) || (n3 < 0 || n3 > 255) || (n4 < 0 || n4 > 255)) {
-        printf("Each octet has to have value between 0 and 255.\n");
-        // printf("Trying to resolve domain name...");
-        exit(EXIT_FAILURE);
-    }
-    return dstIp4;
-    // Dodaj konwertację char * na uint32_t za pomocą komendy inet_pton
-}
-
-// Function to send ICMP Echo messages 
-int sendEchoMess() { 
-    struct sockaddr_in dstSock;
-    int raw_socket;
-    // Filling structs with proper data
-    // Dodaj structa in_addr do którego włozysz adres docelowy z getIpAddress
-    dstSock.sin_family = AF_INET;
-    dstSock.sin_port = 7;
-    dstSock.sin_addr = &
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = 0;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_addr = &dstSock;
-    // Create a RAW socket
-    raw_socket = socket(AF_INET, SOCK_RAW, );
->>>>>>> cf85765d2127e4aefd8a7d227c60e219b1c35300
 }
 
 int main(int agrc, char *argv[]) {
