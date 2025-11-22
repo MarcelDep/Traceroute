440210c86195708d76b3af6bdd5804409a3e5135
Marcel Depta <marceld2005@gmail.com>
2025-11-22 19:37:46 +0100

    created a file just for IP header checksum calculation

--- calchecksum.c
+++ calchecksum.c
@@ -1,29 +1,3 @@
-/* This project is implementation of popular computer network protocol called traceroute. 
-But what traceroute is? It's basically a protocol that trace packet's route. When packet is 
-unpacked by a router then it sends a signal back to host that send original packet. More specifically
-traceroute utulize TTL (time to live) function that when packet's is TTL = 0 then router sends back ICMP
-"Time Exceeded" message to source host. So traceroute sends packet to chose host starting with TTL = 0 
-and incrise its value for each "Time Exceeded" message (this means for each router).*/
-
-// https://beej.us/guide/bgnet/html/#getaddrinfoprepare-to-launch https://man7.org/linux/man-pages/man3/getaddrinfo.3.html https://man7.org/linux/man-pages/man3/sockaddr.3type.html
-
-/* 
-To properly send ICMP packets that are crucial for traceroute implementation I had to implement this 10 steps:
-    1.  Create a RAW socket for specifying ICMP protocol
-    2.  Resolve the hostname to an address if needed (DNS)
-    3.  Create ICMP packet of type 8 (Echo) 
-        3.1. Fill the payload (optional)
-        3.2. Calculate the checksum
-    4.  Record the start time
-    5.  Send out the packet 
-    6.  Wait for a ICMP type 0 (Reply)
-    7.  Record the end time
-    8.  Check the Id byte to make sure it is a reply to our Echo packet
-    9.  If the Id byte is not a match, wait for another packet if the timeout was not reached yet
-    10. Print the result
-I found these steps in the following link: https://github.com/janwilmans/explain_icmp_ping (in README.md file)
-*/
-
 #define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <string.h>
@@ -65,9 +39,6 @@ struct IpHeader {
 } ipHeader;
 int binIPHeader[WHOLE_DATA_IN_IP_PACKET];
 
-// CTRL + ALT + Strzałka -> dodaj nowy kursor
-// CTRL + D -> dodaj kursor przy matchującym wzorcu
-// CTRL + R -> mądry znajdź i zamień (refactor)
 
 // Function to convert decimal numbers into their binary representation
 void decimal_ip_pkt_to_binary(int number) {
@@ -127,11 +98,21 @@ void sum_bits_in_checksum_calculation() {
     }
 }
 
-// Function to calculate checksum in ICMP
-uint16_t calculate_icmp_checksum(int type, int code) {
+// Function to calculate checksum in IP
+uint16_t calculate_checksum() {
     // 1. Change all numbers into binary representation
-    decimal_ip_pkt_to_binary(type);
-    decimal_ip_pkt_to_binary(code);
+    decimal_ip_pkt_to_binary(ipHeader.version);
+    decimal_ip_pkt_to_binary(ipHeader.IHL);
+    decimal_ip_pkt_to_binary(ipHeader.TOS);
+    decimal_ip_pkt_to_binary(ipHeader.TotalLenght);
+    decimal_ip_pkt_to_binary(ipHeader.Identification);
+    decimal_ip_pkt_to_binary(ipHeader.Flags);
+    decimal_ip_pkt_to_binary(ipHeader.FragmentOffset);
+    decimal_ip_pkt_to_binary(ipHeader.TTL);
+    decimal_ip_pkt_to_binary(ipHeader.Protocol);
+    decimal_ip_pkt_to_binary(ipHeader.HeaderChecksum);
+    decimal_ip_pkt_to_binary(ipHeader.SourceAddress);
+    decimal_ip_pkt_to_binary(ipHeader.DestinationAddress);
     // 2. Add bits separeted by 16 bits to create one array of 16 bits
     sum_bits_in_checksum_calculation();
     // 3. Cut additional bits, and add them at the start. Additionally revert 0 to 1 and 1 to 0 
@@ -152,107 +133,13 @@ uint16_t calculate_icmp_checksum(int type, int code) {
     // 4. Convert number into decimal
     uint16_t decimalChecksum;
     int power_of_2 = 0;
-    for (int j = 19; j <= 4; j++) {
+    for (int j = 15; j <= 0; j++) {
         decimalChecksum += checksum[j] * 2^(power_of_2);
         power_of_2++;
     }
     // 4. Put the result intp Header Ckecksum field in struct 
     ipHeader.HeaderChecksum =  decimalChecksum;
     return(decimalChecksum);
-}
-
-// Function to send ICMP Echo messages 
-void send_echo_mess(const char * hostname) { 
-    // Destination IP address will be resolved with getaddrinfo() function
-    struct addrinfo hint; 
-    struct addrinfo *result;
-    // We have to allocate proper amount of memory to our hint struct - we fill whole struct with 0's, and fill it with proper data
-    memset(&hint, 0, sizeof(hint));
-    hint.ai_family = AF_UNSPEC;
-    hint.ai_socktype = SOCK_RAW;
-    hint.ai_protocol = IPPROTO_ICMP;
-    hints.ai_flags = AI_PASSIVE;
-    // The rest of struct is 0
-
-    // Step 1. Resolve the hostname to an address if needed (DNS)
-    int status = getaddrinfo(hostname, NULL, &hint, &result);
-    if (status != 0) {
-        printf("An error has occoured: %d\n", status);
-        exit(EXIT_FAILURE);
-    }
-
-    struct addrinfo *tmp = result;
-    void *addr;
-    char address_string[INET6_ADDRSTRLEN];
-
-    if (tmp->ai_family == AF_INET) {
-        addr = &((struct sockaddr_in*)tmp->ai_addr)->sin_addr;
-    }
-
-    if (tmp->ai_family == AF_INET) {
-        addr = &((struct sockaddr_in6*)tmp->ai_addr)->sin6_addr;
-    }
-
-    inet_ntop(tmp->ai_family, addr, address_string, sizeof(address_string));
-    
-    // If DNS reseolved hostname into an IP address then, we have to write it as a dst IP address in our struct
-    if (address_string != NULL) {
-        hint.ai_addr = address_string;
-    }
-
-    // Step 2. Create a RAW socket for specifying ICMP protocol
-    int sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (sockfd == -1) {
-        printf("SOCKET ERROR !! %d\n\r", sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    // Step 3. Creatign an ICMP packet of type 8
-    struct icmppkt {
-    uint8_t type;
-    uint8_t code;
-    u_int16_t checksum;
-    u_int32_t data;
-    } icmppkt;
-
-    icmppkt.type = 0; // Echo reply
-    icmppkt.code = 0; // Code for echo replies 
-    icmppkt.checksum = calculate_checksum(icmppkt.type, icmppkt.code);
-
-    int connect_reply = connect(sockfd, result->ai_addr, result->ai_addrlen);
-    if (connect_reply == -1) {
-        printf("CONNECT ERROR !! %d\n\r", connect_reply);
-        exit(EXIT_FAILURE);
-    }
-
-    // 6.  Wait for a ICMP type 0 (Reply)
-
-
-    // 4.  Record the start time
-    clock_t start;
-    start = clock();
-
-    // 5.  Send out the packet 
-
-    // We will stop sending packages if it's received by destination address
-
-
-    // 7.  Record the end time
-    clock_t end;
-    end = clock();
-    
-    // 8.  Check the Id byte to make sure it is a reply to our Echo packet
-    
-
-
-    // 9.  If the Id byte is not a match, wait for another packet if the timeout was not reached yet
-
-    
-    // 10. Print the result
-
-
-    // At the end we have to clean up result struct info
-    freeaddrinfo(result);
 }
 
 int main(int agrc, char *argv[]) {
